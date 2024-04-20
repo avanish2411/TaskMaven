64eb38e4cce862d383fb90df5129a7cd8d1aec06
avanish2411 <avanishsingh2411@gmail.com>
2024-04-20 19:20:50 +0530

    added animation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import { View, Text, LogBox } from 'react-native'
 import React from 'react'
 import AppNavigation from './src/Navigation/AppNavigation'
-import Login from './src/Authentication/Register'
 
 export default function App() {
   return (
     <>
       <AppNavigation />
-      {/* <Login /> */}
     </>
   )
 }
--- src/Navigation/AppNavigation.js
+++ src/Navigation/AppNavigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ModalPortal} from 'react-native';
+import { ModalPortal } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../Screens/HomeScreen';
