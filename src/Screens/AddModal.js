1d525809168ac2d3b939e7f0d793f4ce203a3ed1
avanish2411 <avanishsingh2411@gmail.com>
2024-04-21 15:20:53 +0530

    profile and added new component

--- src/Screens/AddModal.js
+++ src/Screens/AddModal.js
@@ -1,10 +1,46 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text , Modal, TextInput, TouchableOpacity,} from 'react-native'
 
-export default function AddModal() {
+export default function AddModal({ modalVisible, setModalVisible, addTodo, title, setTitle, detail, setDetail }) {
     return (
-        <View>
-            <Text>AddModal</Text>
-        </View>
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => setModalVisible(false)}
+        >
+            <View style={{ flex: 1, justifyContent: 'flex-end', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+                <View style={{ backgroundColor: 'white', padding: 20, height: 500 }}>
+                    <Text style={{ color: 'black', fontWeight: '600', fontSize: 24, marginBottom: 10 }}>Title</Text>
+                    <View style={{ backgroundColor: 'grey', marginBottom: 10, borderRadius: 16 }}>
+                        <TextInput
+                            placeholder="Write title.."
+                            placeholderTextColor='black'
+                            value={title}
+                            onChangeText={(text) => setTitle(text)}
+                            style={{ paddingLeft: 16, marginBottom: 10, borderBottomWidth: 1, borderBottomColor: 'gray', color: "white", fontWeight: '400' }}
+                        />
+                    </View>
+                    <Text style={{ color: 'black', fontWeight: '600', fontSize: 24, marginBottom: 10 }}>Details</Text>
+                    <View style={{ backgroundColor: 'grey', borderRadius: 16 }}>
+                        <TextInput
+                            placeholder="Write Details"
+                            placeholderTextColor='black'
+                            value={detail}
+                            onChangeText={(text) => setDetail(text)}
+                            style={{ paddingLeft: 16, marginBottom: 10, borderBottomWidth: 1, borderBottomColor: 'gray', color: "white", fontWeight: '400' }}
+                        />
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: "center", marginBottom: 10 }}>
+                        <TouchableOpacity style={{ backgroundColor: 'blue', paddingHorizontal: 30, paddingVertical: 10, borderRadius: 5, marginTop: 10 }} onPress={addTodo}>
+                            <Text style={{ color: 'white', textAlign: 'center', color: 'white', fontWeight: 'bold', fontSize: 16 }}>Add</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{ backgroundColor: 'red', paddingHorizontal: 30, paddingVertical: 10, borderRadius: 5, marginTop: 10 }} onPress={() => setModalVisible(false)}>
+                            <Text style={{ color: 'white', textAlign: 'center', color: 'white', fontWeight: 'bold', fontSize: 16 }}>Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </Modal>
     )
 }
--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -1,10 +1,49 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Profile() {
-    return (
-        <View>
-            <Text>Profile</Text>
+  const [completedTasks, setCompletedTasks] = useState(0)
+  const [pendingTasks, setPendingTasks] = useState(0)
+
+  const fetchTaskData = async () => {
+    try {
+      const response = await axios.get("http://162.16.2.190:3000/todos/count")
+      const { totalCompletedTodos, totalPendingTodos } = response.data;
+      setCompletedTasks(totalCompletedTodos);
+      setPendingTasks(totalPendingTodos);
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTaskData();
+  }, [])
+  console.log("completed", completedTasks);
+  console.log("pending", pendingTasks);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: 'white' ,padding:8}}>
+      <View>
+        <Text style={{color:'black',fontWeight:'600',fontSize:24}}>Tasks Overview</Text>
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 6, marginVertical: 8, }}>
+          <View style={{ backgroundColor: "#89CFF0", padding: 10, borderRadius: 8, flex: 1, justifyContent: "center", alignItems: "center", }}>
+            <Text style={{ textAlign: "center", fontSize: 16, fontWeight: "bold" ,color:'black',fontSize:14,}}>
+              {completedTasks}
+            </Text>
+            <Text style={{ marginTop: 4,color:'black',fontSize:16 }}>Completed tasks</Text>
+          </View>
+
+          <View style={{ backgroundColor: "#89CFF0", padding: 10, borderRadius: 8, flex: 1, justifyContent: "center", alignItems: "center", }}>
+            <Text style={{ textAlign: "center", fontSize: 16, fontWeight: "bold" ,color:'black',fontSize:14,}}>
+              {pendingTasks}
+            </Text>
+            <Text style={{ marginTop: 4,color:'black',fontSize:16 }}>Pending tasks</Text>
+          </View>
         </View>
-    )
+      </View>
+
+    </View>
+  )
 }
